9e9fda70036e8b96f77897e16f57b85f5aa0c7a4
Jordan Rhode <2262659+JordanRhode@users.noreply.github.com>
2022-03-02 08:28:12 -0600

    setup F# console app

---
+++ src/ConsoleApp/Program.fs
@@ -0,0 +1,13 @@
+﻿open Library
+
+[<EntryPoint>]
+let main args =
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+
+    let value, json = getJson {| args=args; year=System.DateTime.Now.Year; foo="bar" |}
+    
+    // https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/plaintext-formatting
+    printfn $"Input: %0A{value}" // Print width 0 results in a single line of text
+    printfn $"Output: %s{json}"
+
+    0 // return an integer exit code
